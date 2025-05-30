44fe341851d7ceff43f448af9fdbed7f6618b12f
cyril-egbobiani <116189048+cyril-egbobiani@users.noreply.github.com>
2025-05-30 09:59:35 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import { useState } from "react";
-import Navbar from "./components/Navbar";
+ import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
 import FocusArea from "./components/FocusArea";
 import UpcomingEvent from "./components/UpcomingEvent";
@@ -7,8 +6,7 @@ import PastEvents from "./components/PastEvents";
 import Organizers from "./components/Organizers";
 
 function App() {
-  const [count, setCount] = useState(0);
-
+ 
   return (
     <>
       <Navbar />
